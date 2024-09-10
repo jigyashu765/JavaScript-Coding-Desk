6f68c4498f15795019023668f3b8c0923e0e7751
Jigyashu Kumar Gupta <75210289+jigyashu765@users.noreply.github.com>
2024-09-10 17:02:28 +0530

    Add files via upload

---
+++ 8th day/function.js
@@ -0,0 +1,21 @@
+//This functio not return value itt comlte the whole task.
+// function add(a, b){
+//     console.log(a+b);
+// }
+
+// add(5, 2);
+
+// This function resturn some value as the argumnets be pass
+// function add1(a, b){
+//     return(a+b);
+// }
+
+// var a = add1(5, 2);
+// console.log(a + 2);
+
+//The function which have no name is known as anonymous function;
+var a = function(a,b){
+    console.log(a+b);
+}
+
+a(5, 2);

d150964b446437722a0324f6c72bf8b96d941109
Jigyashu Kumar Gupta <75210289+jigyashu765@users.noreply.github.com>
2024-09-11 17:12:09 +0530

    Add files via upload

---
+++ 9th day/ES6.js
@@ -0,0 +1,19 @@
+// ES = ECMA Script;
+// ES 6 = ECMA Script 6(version);
+
+// ECMA = European Computer manufactore Assioction.
+
+//It is an stander for write javascript. it comes in 2015 with 6th and major edition off ecma scrpit languge of javascript.
+
+//ES6 New features :-
+    // Let and const keyword
+    // Arrow Function
+    // Defult parameters
+    // Teplet literals and multipal Lines string
+    // Desturcturing
+    // High order function
+    // Promises
+    // Classes
+    // Modules
+
+    
---
+++ 9th day/hosting.js
@@ -0,0 +1,12 @@
+console.log(a);
+greet();
+
+
+var a = "Hello From A";
+
+function greet() {
+    console.log('Hello From Function');
+}
+
+
+
